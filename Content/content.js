9813834bfcf42074e2c1efc0a3729f51c80ddce5
Jorge Pineda <jpineda@student.neumont.edu>
2018-11-10 16:47:20 -0700

    Removed amazon integration.

--- Content/content.js
+++ Content/content.js
@@ -2,10 +2,6 @@ var url = window.location.href;
 if (url.includes("checkout")) {
     setTimeout(() => {
         var moneyAmount = document.getElementsByClassName("ltrOverride")[0].innerText;
-        // console.log(moneyAmount);
-        // xmlHttp = new XMLHttpRequest();
-        // xmlHttp.open( "GET", chrome.runtime.getURL ("Views/overlay.html"), false );
-        // xmlHttp.send( null );
 
         $.get(chrome.runtime.getURL('Views/overlay.html'), function (data) {
             var inject = document.createElement("div");
@@ -23,7 +19,6 @@ if (url.includes("checkout")) {
             document.body.insertBefore(inject, document.body.firstChild);
 
             inject.querySelector("#donation-slider").addEventListener("input", (event) => {
-                console.log(event.path[0].value);
                 $("#stickDiv").html("")
                 for (i = 0; i < event.path[0].value; i++) {
                     if(i %2 == 0){
@@ -41,17 +36,58 @@ if (url.includes("checkout")) {
 
             inject.querySelector("#close").addEventListener("click", (event) => {
                 $(".overlay-root").remove();
-                console.log("SUP")
             });
         });
 
     }, 1000);
 }
 
-else if (url == "https://www.amazon.com/gp/buy/spc/handlers/display.html?hasWorkingJavascript=1") {
-    var amazonMoneyAmount = document.getElementsByClassName("grand-total-price")[0].innerText;
-    console.log(amazonMoneyAmount);
-}
+// else if (url == "https://www.amazon.com/gp/buy/spc/handlers/display.html?hasWorkingJavascript=1") {
+//     console.log(moneyAmount);
+    
+//     setTimeout(() => {
+//         var moneyAmount = document.getElementsByClassName("grand-total-price")[0].innerText;
+//         $.get(chrome.runtime.getURL('Views/overlay.html'), function (data) {
+//         console.log(data);
+//         var inject = document.createElement("div");
+//         inject.classList.add("overlay-root");
+//         inject.innerHTML = data;
+//         var totalMoneyDouble = moneyAmount.substring(moneyAmount.indexOf('$') + 1);
+//         inject.querySelector("#totalSpent").innerText = totalMoneyDouble;
+//         var initialDonation = (100 - totalMoneyDouble.substring(totalMoneyDouble.indexOf('.') + 1)) * 0.01;
+//         console.log(initialDonation);
+//         inject.querySelector("#initialDonation").innerText = initialDonation;
+//         var initialFedStudents = Math.ceil(initialDonation / .50);
+//         console.log(initialFedStudents);
+//         inject.querySelector("#initialFedStudents").innerText = Math.ceil(initialDonation / .50);
+
+//         inject.querySelector("#donation-slider").value = initialFedStudents;
+
+//         document.body.insertBefore(inject, document.body.firstChild);
+
+//         inject.querySelector("#donation-slider").addEventListener("input", (event) => {
+//             $("#stickDiv").html("")
+//             for (i = 0; i < event.path[0].value; i++) {
+//                 if(i %2 == 0){
+//                     $("#stickDiv").append('<img src="https://cdn2.iconfinder.com/data/icons/little-girl/512/little_girl_grey.png" width = "25px" height="25px"/>');
+//                 }else{
+//                     $("#stickDiv").append('<img src="https://image.flaticon.com/icons/png/512/146/146010.png" width = "25px" height="25px"/>');
+//                 }
+//             }
+//             var donation = .50 * event.path[0].value;
+//             $('#initialDonation').text(donation.toFixed(2));
+//             $('#initialFedStudents').text(Math.ceil(donation / .50));
+//         });
+
+//         inject.querySelector("#payPalImg").addEventListener("click", (event) => donateButtonClick(event));
+
+//         inject.querySelector("#close").addEventListener("click", (event) => {
+//             $(".overlay-root").remove();
+//         });
+//     });
+    
+// }, 1000);
+// }
 
 
 function donateButtonClick(event) {
@@ -69,5 +105,6 @@ function donateButtonClick(event) {
             $(".overlay-content").html(htmlStr);
         })
     });
+    
 }
 
