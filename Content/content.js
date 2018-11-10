4d46458323ed60428cac93e0be81fe8d07c49b51
Jorge Pineda <jpineda@student.neumont.edu>
2018-11-10 09:51:05 -0700

    manifest.json updated.content.js is adding total to the overlay.html.

--- Content/content.js
+++ Content/content.js
@@ -3,7 +3,15 @@ if (url.includes("checkout")) {
     setTimeout(() => {
         var moneyAmount = document.getElementsByClassName("ltrOverride")[0].innerText;
         console.log(moneyAmount);
-    }, 1000);   
+        xmlHttp = new XMLHttpRequest();
+        xmlHttp.open( "GET", chrome.runtime.getURL ("Views/overlay.html"), false );
+        xmlHttp.send( null );
+        
+        var inject  = document.createElement("div");
+        inject.innerHTML = xmlHttp.responseText;
+        inject.querySelector("#totalSpent").innerText = moneyAmount;
+        document.body.insertBefore (inject, document.body.firstChild);
+    }, 1000);
 }
 
 else if (url == "https://www.amazon.com/gp/buy/spc/handlers/display.html?hasWorkingJavascript=1") {
