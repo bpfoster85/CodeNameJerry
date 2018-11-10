a0becec8e3c5af14249a04f3449a5829738094ab
Bryan Paul Foster <bpfoster85@gmail.com>
2018-11-10 14:11:55 -0700

    styling improvements

--- Content/DBTrans.js
+++ Content/DBTrans.js
@@ -7,7 +7,14 @@ function SaveOneTimeDonation(amt) {
     var don = new Donation(1, amt);
     this.MainSave(don);
 }
-
+function SaveWeeklyDonation(amt) {
+    var don = new Donation(2, amt);
+    this.MainSave(don);
+}
+function SaveMonthlyDonation(amt) {
+    var don = new Donation(3, amt);
+    this.MainSave(don);
+}
 function MainSave(donation) {
     chrome.storage.sync.get("DonationList", function (result) {        
         result.DonationList.Donations.push(donation);
@@ -16,7 +23,6 @@ function MainSave(donation) {
     });
 });    
 }
-
 function GetAllDonations() {    
     chrome.storage.sync.get("DonationList", function (result) {
         console.log(result);
--- Content/popup.js
+++ Content/popup.js
@@ -13,7 +13,7 @@ $(function () {
     $("#tshare").click(function () {
         alert("Share link to install chrome extension on Twitter")
     });
-    $("#donation-slider").onInput(function(event) {
+    $("#donation-slider").on('input',function(event) {
         $("#stickDiv").html("");
         for (i = 0; i < event.path[0].value; i++) {
             $("#stickDiv").append('<img src="https://static.thenounproject.com/png/203593-200.png" width = "20px" height="20px"/>');
