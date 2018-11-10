39d0e67e564ddded31ae876f55daa14916846b7e
Bryan Paul Foster <bpfoster85@gmail.com>
2018-11-10 15:32:39 -0700

    contact form

--- Content/popup.js
+++ Content/popup.js
@@ -1,11 +1,11 @@
-$(function () {
 
+$(function () {
+    $('select').formSelect();
     $("#moreinfosection").hide();
-    $("#maincontent").toggle();
-    $("#moreinfosection").toggle();
+    // $("#maincontent").toggle();
+    // $("#moreinfosection").toggle();
     $("#moreinfo").click(function () {
         SwitchView();
-
         if ($('#moreinfosection').is(":visible")) {
             GetReaccuringPaymentData();
         }
@@ -19,6 +19,7 @@ $(function () {
     });
     $("#donation-slider").on('input', function (event) {
         $("#stickDiv").html("");
+        $("#donate").html('<i class="material-icons left">face</i>Donate $' + (event.currentTarget.value * 5).toFixed(2));
         for (i = 0; i < event.currentTarget.value; i++) {
             $("#stickDiv").append('<img src="https://static.thenounproject.com/png/203593-200.png" width = "20px" height="20px"/>');
         }
@@ -26,16 +27,35 @@ $(function () {
     $("#eshare").click(function () {
         alert("Share link to install chrome extension using email")
     });
-    // $("#initdb").click(
-    //     function () {
-    //         SaveOneTimeDonation(123.00);
-    //         GetAllDonations();
-    //     });
-    // $("#viewdata").click(function () {        
-    //     GetAllDonations();
-    // });
-});
+    $("#donate").click(function () {
+        GetUserData();
 
+    })
+
+});
+function GetUserData() {
+    var kidCount = $("#donation-slider").val();
+    var freq = $("#freq").val();
+    var subtotal = kidCount * .50;
+
+    var don;
+    if (freq == 1) {
+        don = new Donation(1, subtotal, new Date());
+    }
+    else if (freq == 2) {
+        don = new Donation(2, subtotal, new Date());
+    }
+    else {
+        don = new Donation(3, subtotal, new Date());
+    }
+    
+    chrome.storage.sync.get("DonationList", function (result) {        
+        result.DonationList.Donations.push(don);
+    chrome.storage.sync.set({ "DonationList": result.DonationList }, function () {        
+        $("#donate").html('<i class="material-icons left">face</i>Donate!');
+    });
+});  
+}
 function SwitchView() {
     $("#maincontent").toggle();
     $("#moreinfosection").toggle();
@@ -47,12 +67,13 @@ function GetReaccuringPaymentData() {
     var tableTemplate = [];
 
     chrome.storage.sync.get("DonationList", function (result) {
-        result.
+        results = result.DonationList.Donations;
         $.each(results, function (i, d) {
-            var dTemplate = "<tr><td></td><td></td><tr>";
-            tableTemplate.push(dTemplate);
+            if (d.DonationType != 1) {
+                var dTemplate = "<tr><td>" + d.Amount + "</td><td>" + d.DonationDate + "</td><tr>";
+                tableTemplate.push(dTemplate);
+            }
         });
-
         $("#reaccuringdatalayout").appendTo(tableTemplate.join(""));
     });
 }
